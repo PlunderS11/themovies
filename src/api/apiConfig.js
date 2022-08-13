5642b2d887c59e51f8c4eccb17c5773f524fedc1
Plun <phuchoangxyz@gmail.com>
2022-08-13 12:04:44 +0700

    .

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'http://api.themoviedb.org/3/',
+    baseUrl: 'https://api.themoviedb.org/3/',
     apikey: '04ecf4702177b605537b94d129a7048d',
     originImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
